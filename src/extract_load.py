1b61995d6d2997b39e2d165a19ac9ac2375e234b
R-midolli <rafaelmidolli@gmail.com>
2024-10-18 22:22:30 -0300

    load the dsta to postgres

--- src/extract_load.py
+++ src/extract_load.py
@@ -3,8 +3,24 @@ import yfinance as yf
 import pandas as pd
 from dotenv import load_dotenv
 import os
+from sqlalchemy import create_engine
 
 #import ambient variables
+load_dotenv()
+
+#import database variables
+DB_HOST = os.getenv('DB_HOST_PROD')
+DB_PORT = os.getenv('DB_PORT_PROD')
+DB_USER = os.getenv('DB_USER_PROD')
+DB_PASSWORD = os.getenv('DB_PASSWORD_PROD')
+DB_NAME = os.getenv('DB_NAME_PROD')
+DB_SCHEMA = os.getenv('DB_SCHEMA_PROD')
+DB_THREADS = os.getenv('DB_THREADS_PROD')
+DB_TYPE = os.getenv('DB_TYPE_PROD')
+
+DB_URL = f"{DB_TYPE}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DB_URL)
 
 # list of commodities to extract
 commodities = ["CL=F","GC=F", "SI=F", "HG=F", "PL=F", "PA=F", "ALI=F"]
@@ -24,7 +40,11 @@ def search_all_data(commodities):
         all_data.append(data)
     return pd.concat(all_data)
 
+#load the data to postgres
+def load_data_to_postgres(df, schema=DB_SCHEMA):
+    df.to_sql('commodities', engine, if_exists='replace', index=True, index_label='Date', schema=schema)
+
 if __name__ == "__main__":
     concatenated_data = search_all_data(commodities)
-    print(concatenated_data)
-
+    load_data_to_postgres(concatenated_data, schema=DB_SCHEMA)
+    print("Data loaded to postgres")
